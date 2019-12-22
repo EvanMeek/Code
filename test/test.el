0f244f423f4e652ca3e5ec478035ddec05e4012f
EvanMeek <the_lty_mail@foxmail.com>
2019-12-23 02:10:51 +0800

    随便提交点代码

---
+++ test/test.el
@@ -0,0 +1,10 @@
+(car '(+ 1 (+ 2 3)))
+fill-column
+(+ 2 2)
+
+(concat "My name is:" "EvanMeek")
+(concat "Nice to meet " (substring "fuck you" 5 8))
+(message "Should you sleep now?")
+(message "The name of this buffer is: %s" (buffer-name))
+(message "The value of fill-column is %d" fill-column)
+(message "我想吃%s，各来%d斤!" (concat "苹果、" "梨子、" "葡萄") (+ 1 1))
